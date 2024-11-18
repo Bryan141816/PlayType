38407b621af462f583d1db16179c75e47b9542fb
Gary Antier <65901995+sagemodeninja@users.noreply.github.com>
2024-11-18 22:37:59 +0800

    Create min-settings.py

---
+++ type_master/type_master/min-settings.py
@@ -0,0 +1,15 @@
+from type_master.settings import *  # Import base settings
+
+# Disable database connections
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',  # Use a dummy database engine
+    }
+}
+
+# Optional: Disable middleware, authentication backends, etc.
+MIDDLEWARE = []
+
+# Keep only the necessary static file settings
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
