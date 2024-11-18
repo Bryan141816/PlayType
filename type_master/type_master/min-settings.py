755fb2b732b00a3f978b824bca6495e089b0dc5d
Gary Antier <65901995+sagemodeninja@users.noreply.github.com>
2024-11-18 23:33:43 +0800

    Update min-settings.py

--- type_master/type_master/min-settings.py
+++ type_master/type_master/min-settings.py
@@ -1,8 +1,4 @@
-#from type_master.settings import *  # Import base settings
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from type_master.settings import *  # Import base settings
 
 # Disable database connections
 DATABASES = {
@@ -10,9 +6,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.dummy',  # Use a dummy database engine
     }
 }
-
-# Optional: Disable middleware, authentication backends, etc.
-MIDDLEWARE = []
 
 # Keep only the necessary static file settings
 STATIC_URL = '/static/'
