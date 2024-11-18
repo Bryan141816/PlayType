5defea691c08410c431fc2bdc0a20a2177b602e6
Gary Antier <65901995+sagemodeninja@users.noreply.github.com>
2024-11-18 23:29:15 +0800

    Update min-settings.py

--- type_master/type_master/min-settings.py
+++ type_master/type_master/min-settings.py
@@ -1,5 +1,8 @@
 #from type_master.settings import *  # Import base settings
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Disable database connections
 DATABASES = {
     'default': {
