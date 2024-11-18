d971cdd116df48baf0fbf5b70d71fbeb1b29d1db
Gary Antier <65901995+sagemodeninja@users.noreply.github.com>
2024-11-18 23:30:32 +0800

    Update min-settings.py

--- type_master/type_master/min-settings.py
+++ type_master/type_master/min-settings.py
@@ -1,4 +1,5 @@
 #from type_master.settings import *  # Import base settings
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
